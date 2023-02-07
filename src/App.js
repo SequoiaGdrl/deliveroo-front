59682c40852be55aeeb2a5195c2776c6ea9b00ed
sequoiaGrdl <sequoiagendrel@gmail.com>
2023-02-07 18:45:57 +0100

    panier v2

--- src/App.js
+++ src/App.js
@@ -1,152 +1,140 @@
-import './App.css';
-import {useState, useEffect} from "react"
+import "./App.css";
+import { useState, useEffect } from "react";
 import axios from "axios";
-import logo from './assets/images/logo-teal.svg';
-import Card from './components/Card';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus,faMinus } from '@fortawesome/free-solid-svg-icons'
-
+import logo from "./assets/images/logo-teal.svg";
+import Card from "./components/Card";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
-  const [data, setData]=useState();
-  const [isLoading, setIsLoading] = useState(true);
-  const [tab, setTab]=useState([]);
+	const [data, setData] = useState();
+	const [isLoading, setIsLoading] = useState(true);
+	const [tab, setTab] = useState([]);
 
-    
+	useEffect(() => {
+		const getData = async () => {
+			const response = await axios.get(
+				"https://site--backend-deliveroo--y4khgqdvwnm7.code.run/"
+			);
+			setData(response.data);
+			setIsLoading(false);
+		};
+		getData();
+	}, []);
 
+	const handleCounterPlus = (index) => {
+		const newTab = [...tab];
+		newTab[index].counter++;
+		setTab(newTab);
+	};
 
+	const handleCounterMoins = (index) => {
+		const newTab = [...tab];
 
+		if (newTab[index].counter > 1) {
+			newTab[index].counter--;
+		} else {
+			newTab.splice(index, 1);
+		}
 
+		setTab(newTab);
+	};
 
-useEffect(()=>{
-  const getData = async()=>{
-  const response = await axios.get("https://site--backend-deliveroo--y4khgqdvwnm7.code.run/")
-  setData(response.data);
-  setIsLoading(false)
-  }
-  getData();
- 
+	return (
+		<div className="App">
+			{isLoading ? (
+				"loading..."
+			) : (
+				<>
+					<header>
+						<div className="container">
+							<img src={logo} alt="" />
+						</div>
+					</header>
+					<main>
+						<section className="grandContainer">
+							<div className="first">
+								<h1>{data.restaurant.name}</h1>
+								<p>{data.restaurant.description}</p>
+							</div>
 
-},[])
+							<div className="second">
+								<img src={data.restaurant.picture} alt="" />
+							</div>
+						</section>
 
+						<section className="containerMain">
+							<section className="container2">
+								{data.categories.map(
+									(cat, index) =>
+										cat.meals.length > 1 && (
+											<section className="containerCat">
+												<h1>{cat.name}</h1>
+												<section>
+													{cat.meals.map((meal, index) => {
+														return (
+															<Card
+																index={index}
+																meal={meal}
+																tab={tab}
+																setTab={setTab}
+															/>
+														);
+													})}
+												</section>
+											</section>
+										)
+								)}
 
-const handleCounterPlus= (index)=>{
+								<div>
+									<p>reztgehg</p>
+								</div>
+							</section>
+							<section className="panier">
+								<div className="suPanier">
+									<div className="boutonPanier">
+										<button>Valider mon panier</button>
+									</div>
+									<div className="textPanier">
+										{tab.map((elem, index) => {
+											return (
+												<div className="icon-meal" key={index}>
+													<div>
+														<span
+															className="icon"
+															onClick={() => {
+																handleCounterMoins(index);
+															}}
+														>
+															<FontAwesomeIcon icon={faMinus} />
+														</span>
+														<span>{elem.counter}</span>
+														<span
+															className="icon"
+															onClick={() => {
+																handleCounterPlus(index);
+															}}
+														>
+															<FontAwesomeIcon icon={faPlus} />
+														</span>
+														<span className="meal">{elem.meal.title}</span>
+													</div>
 
-  
-let result= tab[index].counter;
-
-console.log(result)
-
-
-
-
-
-
+													<div>
+														<span className="price"> {elem.meal.price}€</span>
+													</div>
+												</div>
+											);
+										})}
+									</div>
+								</div>
+							</section>
+						</section>
+					</main>
+				</>
+			)}
+		</div>
+	);
 }
-
-  return (
-    <div className="App">
-{isLoading ? 
-("loading...") : 
-(
-<>
-<header>
-  <div className='container'>
-    <img src={logo} alt="" />
-  </div> 
-</header>
-<main>
-<section className="grandContainer">
-    <div className='first'>
-      <h1>{data.restaurant.name}</h1>
-      <p>{data.restaurant.description}</p>
-    </div>
-
-    <div className='second'>
-      <img src={data.restaurant.picture} alt="" />
-
-    </div>
-  </section>
-
-<section className='containerMain'>
-  <section className='container2'>
-  {data.categories.map((cat, index)=>(
-      
-        cat.meals.length > 1 && (<section className='containerCat'>
-            <h1>{cat.name}</h1>
-            <section> 
-              {cat.meals.map((meal, index)=>{
-                return(
-                  <Card index={index}  meal={meal} tab={tab} setTab={setTab}/>
-                )
-              })}     
-            
-            </section>
-
-            
-
-          </section>     
-        
-        )
-
-        ))}
-        
-        <div> 
-              <p>reztgehg</p>
-            </div>
-  </section>
-  <section className='panier'>
-    <div className='suPanier'> 
-      <div className='boutonPanier'><button>Valider mon panier</button></div>
-      <div className='textPanier'>
-        
-        {tab.map((elem,index)=>{
-
-
-          return(
-             <div className='icon-meal' key={index}>
-          <div>
-          <span className='icon' onClick={()=>{handleCounterPlus(index)}}>< FontAwesomeIcon icon={faMinus} /></span>
-          <span>{elem.counter}</span>
-          <span className='icon'>< FontAwesomeIcon icon={faPlus} /></span>
-          <span className='meal'>{elem.meal.title}</span>
-          </div>
-          
-          <div>
-            <span className='price'> {elem.meal.price}€</span>
-          </div>
-        </div>
-          )
-          
-
-        })}
-       
-       
-        
-        </div>
-        
-    </div>
-    
-  </section>
-  {console.log(tab)}
-</section>
-</main>
-
-
-</>
-)
-}
-
-     
-    </div>
-  );
-}
-
-        
-
-    
-
-  
-
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,32 +1,42 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
-const Card =({index,meal,tab,setTab})=>{
+const Card = ({ index, meal, tab, setTab }) => {
+	return (
+		<div
+			className="card"
+			key={index}
+			onClick={() => {
+				const newTab = [...tab];
 
-    return (
-        <div className='card' key={index} onClick={() => {
-const newTab=[...tab];
-newTab.push({meal:meal,counter:1})
-console.log(newTab);
-setTab(newTab)
+				const mealExist = newTab.find((elem) => elem.meal.title == meal.title);
+				if (mealExist) {
+					mealExist.counter++;
+				} else {
+					newTab.push({ meal: meal, counter: 1 });
+				}
 
+				setTab(newTab);
+			}}
+		>
+			<div>
+				<p className="title">{meal.title}</p>
+				<p className="subTitle">{meal.description}</p>
+				<span>{meal.price} €</span>
+				{meal.popular && (
+					<span className="populaire">
+						<FontAwesomeIcon icon={faStar} />
+						Populaire
+					</span>
+				)}
+			</div>
+			<div>
+				{meal.picture && (
+					<img width={130} height={130} src={meal.picture} alt="" />
+				)}
+			</div>
+		</div>
+	);
+};
 
-
-        }}>
-        <div>
-          <p className='title'>{meal.title}</p>
-          <p className='subTitle'>{meal.description}</p>
-          <span>{meal.price} €</span>
-          {meal.popular&& <span className="populaire"><FontAwesomeIcon icon={faStar}/>Populaire</span>}
-          
-        </div>
-        <div>
-          {meal.picture &&  (<img width={130} height={130} src={meal.picture} alt=''/>)}
-        
-        </div>      
-      </div>
-    )
-
-}
-
-export default Card
+export default Card;
